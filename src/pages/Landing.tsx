e85f33729bb246df1bf513d7e08d887d967382e9
Carlos Borja <carlosborjawebdev@gmail.com>
2026-01-03 00:39:00 +0100

    Add initial layout of landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,13 @@
-
-
+import { Heading, Button, VStack, Textarea } from "@chakra-ui/react"
+import { LuChartNoAxesColumn, LuArrowRight } from "react-icons/lu";
 const Landing = () =>{
 
 return(
-    <div>
-        <h1>Home</h1>
-    </div>
+    <VStack h="100vh" w="100vw" px={10} justifyContent="center" alignItems="center" bgColor="blue.900" >
+        <Heading fontSize="4xl">Leaderboards</Heading>
+        <Textarea my={10} minH="40" colorPalette="orange" placeholder="Paste your score here!" />
+        <Button bgColor="orange.solid" color="white" fontWeight="semibold"><LuChartNoAxesColumn />Show me the leaderboards <LuArrowRight /></Button>
+    </VStack>
 )
 }
 
