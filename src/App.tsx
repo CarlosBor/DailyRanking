93552966e2db473367f8dddc70a6905f2495c0a2
Carlos Borja <carlosborjawebdev@gmail.com>
2026-01-03 00:12:46 +0100

    Initial commit, add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import Landing from "@/pages/Landing"
+import Leaderboard from "@/pages/Leaderboard";
 
 const App = () => {
 
   return (
-    <>
-      <p>Start work here</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
